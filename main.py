bd2619f1bcf074dd2e2cc5493cda72308bc5461a
dmontelucas <dmonte.lucas.r@gmail.com>
2025-10-22 14:54:57 +1100

    final changes and refactoring for bootdev assignment, code takes dynamic inputs within the books file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def get_book_text():
-    with open("books/frankenstein.txt", "r") as f:
+import sys
+from stats import word_count, character_count
+
+def get_book_text(filename):
+    script_dir = sys.path[0]
+    
+    book_path = script_dir + "/" + filename
+    with open(book_path, "r") as f:
         text = f.read()
     return text
 
@@ -9,11 +15,19 @@ def get_book_text():
     #print(book_text)
 
 def main():
-    from stats import word_count
-    from stats import character_count
-    char_dict = character_count()
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filename = sys.argv[1]
+    book_text = get_book_text(filename)
+    num_words = word_count(book_text)
+    print(f"Found {num_words} total words")
+
+    char_dict = character_count(book_text)
     for ch, count in sorted(char_dict.items()):
-        print(f"'{ch}': {count}")
-    #print_book()
+        print(f"{ch}: {count}")
+    
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,27 +1,18 @@
-def get_book_text():
-    with open("books/frankenstein.txt", "r") as f:
-        text = f.read()
-    return text
 
-def word_count():
-    book_text = get_book_text()
-    words = book_text.split()
-    num_words = len(words)
-    print(f"Found {num_words} total words")
+def word_count(text):
+    words = text.split()
+    return len(words)
 
-def character_count():
-    book_text = get_book_text().lower()
+def character_count(text):
+    text = text.lower()
     characters = {}
 
-    for c in book_text:
-        if c in characters:
-            characters[c] += 1
-        else:
-            characters[c] = 1
+    for c in text:
+        characters[c] = characters.get(c, 0) + 1
     return characters
 
-def stats():
-    word_count()
-    character_count()
+#def stats(text):
+   # word_count(text)
+    #character_count(text)
 
-stats()
+#stats()
