bc9eb39f14be4c4dd516c8854dc0ec3e77159966
dmontelucas <dmonte.lucas.r@gmail.com>
2025-10-22 11:53:31 +1100

    main.py word_count and print_book functions

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt", "r") as f:
+        text = f.read()
+    return text
+
+def word_count():
+    book_text = get_book_text()
+    words = book_text.split()
+    num_words = len(words)
+    print(f"Found {num_words} total words")
+
+#def print_book():
+    #book_text = get_book_text()
+    #print(book_text)
+
+def main():
+    #print_book()
+    word_count()
+
+main()
